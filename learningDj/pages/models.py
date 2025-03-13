7478cfc68326d8a5c0e5c32e92f88472409ac546
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2025-03-13 06:02:48 +0200

    models

--- learningDj/pages/models.py
+++ learningDj/pages/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# What is models?
+# How dose the models page work
+
+class Car(models.Model):
+        name= models.CharField(max_length=100)
+        price = models.DecimalField(max_digits=6, decimal_places=2)
