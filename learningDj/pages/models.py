ce12efb4cd8a6b5536b45c96b1a01712a32e3494
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2025-03-13 06:33:10 +0200

    data in database

--- learningDj/pages/models.py
+++ learningDj/pages/models.py
@@ -3,11 +3,5 @@ from django.db import models
 # What is models?
 # How dose the models page work
 
-class Product(models.Model):
-    name = models.CharField(max_length=50)
-    content = models.TextField()
-    price = models.DecimalField(max_digits=5,decimal_places=2)
-    image = models.ImageField(upload_to='photos/%y%m%d')
-    actaive = models.BooleanField(default=True)
-    
+
     
--- learningDj/products/models.py
+++ learningDj/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+# migration mean put this class in django files => python manage.py makemigrations
+# migrate mean put in db => python manage.py migrate
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    content = models.TextField()
+    price = models.DecimalField(max_digits=5,decimal_places=2)
+    image = models.ImageField(upload_to='photos/%y%m%d')
+    actaive = models.BooleanField(default=True)
+    
