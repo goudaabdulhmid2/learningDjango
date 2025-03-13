4b2c118713889c583c2eb3198506d1274a8a357d
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2025-03-13 16:55:37 +0200

    show products on templates

--- learningDj/products/views.py
+++ learningDj/products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def product(request):
+    
+    return render(request,'products/product.html')
+
+def products(request):
+    data = {'pro': Product.objects.all()}
+    return render(request,'products/products.html',data)
