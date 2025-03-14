ca0cb6747ba156c1ab0d3206d6d0a1ee81907349
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2025-03-14 14:13:21 +0200

    query set

--- learningDj/products/views.py
+++ learningDj/products/views.py
@@ -7,5 +7,9 @@ def product(request):
     return render(request,'products/product.html')
 
 def products(request):
-    data = {'pro': Product.objects.all()}
+    # data = {'pro': Product.objects.get(id = 1)}
+    pro = Product.objects.all()
+    data = {'pro': pro.filter(price=99.99)}
+    
+
     return render(request,'products/products.html',data)
