9885f496ee507db63291dff23eb27dac46ad692d
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2025-03-14 14:20:47 +0200

    query set methods

--- learningDj/products/views.py
+++ learningDj/products/views.py
@@ -9,7 +9,10 @@ def product(request):
 def products(request):
     # data = {'pro': Product.objects.get(id = 1)}
     pro = Product.objects.all()
-    data = {'pro': pro.filter(price=99.99)}
+    # data = {'pro': pro.order_by('price')}
+    # data = {'pro': str(pro.count())}
+    data = {'pro': pro.exclude(price=99.99)}
+
     
 
     return render(request,'products/products.html',data)
