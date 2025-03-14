e44ce4fe67778cf2ad6ad92fc690186cf3be3721
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2025-03-14 14:28:43 +0200

    range and in and exact and contains

--- learningDj/products/views.py
+++ learningDj/products/views.py
@@ -7,12 +7,14 @@ def product(request):
     return render(request,'products/product.html')
 
 def products(request):
-    # data = {'pro': Product.objects.get(id = 1)}
-    pro = Product.objects.all()
-    # data = {'pro': pro.order_by('price')}
-    # data = {'pro': str(pro.count())}
-    data = {'pro': pro.exclude(price=99.99)}
-
     
+    pro = Product.objects.all()
+    # data = {'pro':pro.filter(price__exact=99.99)}
+    # data = {'pro':pro.filter(name__exact='Docker')}
+    # data = {'pro':pro.filter(name__contains='A')}
+    # data = {'pro':pro.filter(price__in=[333.99,99.99])}
+    data = {'pro':pro.filter(price__range=(10,500))}
+
+
 
     return render(request,'products/products.html',data)
