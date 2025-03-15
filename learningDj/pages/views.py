395c1a157b743ca4af4729cb9d49152a00f2b816
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2025-03-15 05:51:00 +0200

    create form 2

--- learningDj/pages/views.py
+++ learningDj/pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Login
-
+from .forms import LoginForm
 # Create your views here.
 
 def index(request):
@@ -18,4 +18,4 @@ def about(request):
     data = Login(username=username,password=password)
     data.save();
 
-    return render(request,'pages/about.html');
+    return render(request,'pages/about.html',{'lf':LoginForm});
