25531669c38e5d5726aa3b5d9b0e275a8cfc5f36
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2025-03-15 05:56:09 +0200

    forms values

--- learningDj/pages/forms.py
+++ learningDj/pages/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 
+# labal
+# initial
+# disable = True , false
+# help_text
+# widget
+# required
+
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=50)
-    password = forms.CharField(max_length=50)
+    username = forms.CharField(max_length=50,label='name')
+    password = forms.CharField(max_length=50,widget=forms.PasswordInput)
 
